0fa22f5a3300c7353d1b01c2a21c338753a839e3
sazzad ul alam Shawon <93247296+sazzadulalambd@users.noreply.github.com>
2022-07-02 04:24:43 +0600

    sazzad

---
+++ BePartner_App_Mid/CustomValidation/Admin_EV.cs
@@ -0,0 +1,37 @@
+﻿using BePartner_App_Mid.EF;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BePartner_App_Mid.CustomValidation
+{
+    public class Admin_EV : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if(value != null)
+            {
+                
+                if(HttpContext.Current.Session["Ad_Email"] != null)
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    string message = value.ToString();
+                    var db = new bePartnerCentralDatabaseEntities2();
+                    var Admin = (from I in db.Admins where I.Ad_Email.Equals(message) select I).FirstOrDefault();
+                    if (Admin != null)
+                    {
+                        return new ValidationResult("*Email already exists");
+                    }
+                    return ValidationResult.Success;
+                }
+                
+            }
+            return new ValidationResult("*Something went wrong");
+        }
+    }
+}
