a82df731cf76e9e396d03d53cab6ddc25069bf3d
sazzad ul alam Shawon <93247296+sazzadulalambd@users.noreply.github.com>
2022-07-02 04:23:58 +0600

    sazzad

--- BePartner_App_Mid/Controllers/AD_HomeController.cs
+++ BePartner_App_Mid/Controllers/AD_HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BePartner_App_Mid.EF;
+using BePartner_App_Mid.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,46 +13,126 @@ namespace BePartner_App_Mid.Controllers
         // GET: AD_Home
         public ActionResult Index()
         {
+            ViewBag.AD_Email = Session["Email"].ToString();
             return View();
         }
 
+       
+
+      
         public ActionResult Account()
         {
-            return View();
+            if (Session["Email"] != null)
+            {
+                var email = Session["Email"].ToString();
+                var db = new bePartnerCentralDatabaseEntities2();
+                var AD = (from I in db.Admins where I.Ad_Email.Equals(email) select I).FirstOrDefault();
+                Session["Admin_img"] = AD.Img;
+
+                ViewBag.AD_Img= Session["Admin_img"].ToString();
+                ViewBag.AD_Email = email;
+                return View(AD);
+               
+
+            }
+            return RedirectToAction("login_S", "S_Admin");
+
+            //return View();
         }
-        public ActionResult Logout()
+
+
+
+            public ActionResult Logout()
         {
             return View();
         }
 
         public ActionResult All_Admin()
         {
-            return View();
+            if (Session["Email"] != null)
+            {
+                var db = new bePartnerCentralDatabaseEntities2();
+                var ads = db.Admins.ToList();
+
+                ViewBag.AD_Email = Session["Email"].ToString();
+
+                return View(ads);
+                
+                
+            }
+            return RedirectToAction("login_S", "S_Admin");
         }
+          
 
         public ActionResult All_Employee()
         {
-            return View();
+            if (Session["Email"] != null)
+            {
+                ViewBag.AD_Email = Session["Email"].ToString();
+
+                var db = new bePartnerCentralDatabaseEntities2();
+                var ems = db.Employees.ToList();
+                return View(ems);
+
+            }
+            return RedirectToAction("login_S", "S_Admin");
         }
         public ActionResult All_Investor()
         {
-            return View();
+            if (Session["Email"] != null)
+            {
+                ViewBag.AD_Email = Session["Email"].ToString();
+
+                var db = new bePartnerCentralDatabaseEntities2();
+                var ins = db.Investors.ToList();
+                return View(ins);
+            }
+            return RedirectToAction("login_S", "S_Admin");
         }
         public ActionResult All_Entrepreneur()
         {
-            return View();
+            if (Session["Email"] != null)
+            {
+                ViewBag.AD_Email = Session["Email"].ToString();
+
+                var db = new bePartnerCentralDatabaseEntities2();
+                var ens = db.Entrepreneurs.ToList();
+                return View(ens);
+            }
+            return RedirectToAction("login_S", "S_Admin");
         }
         public ActionResult AD_Notification()
         {
+            ViewBag.AD_Email = Session["Email"].ToString();
+
             return View();
         }
         public ActionResult AD_Settings()
         {
+            ViewBag.AD_Email = Session["Email"].ToString();
+
             return View();
         }
         public ActionResult AD_Messege()
         {
+            ViewBag.AD_Email = Session["Email"].ToString();
+
             return View();
         }
+
+     
+        public ActionResult _AD_Layout()
+        {
+          if (Session["Email"] != null)
+            {
+               
+            }
+            return RedirectToAction("login_S", "S_Admin");
+
+        }
+
+
+
+        
     }
 }
--- BePartner_App_Mid/Controllers/S_AdminController.cs
+++ BePartner_App_Mid/Controllers/S_AdminController.cs
@@ -3,6 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+using System.Web.Security;
+using BePartner_App_Mid.Models;
+using BePartner_App_Mid.EF;
+using static System.Net.WebRequestMethods;
+
+
 
 namespace BePartner_App_Mid.Controllers
 {
@@ -13,19 +23,163 @@ namespace BePartner_App_Mid.Controllers
         {
             return View();
         }
+        [HttpGet]
         public ActionResult Regstration_S1()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Regstration_S1(S_Admin_Model_Reg01 SAMR1)
+        {
+            if (ModelState.IsValid)
+            {
+                Session["FirstName"] = SAMR1.FirstName;
+                Session["LastName"] = SAMR1.LastName;
+                Session["Email"] = SAMR1.Email;
+                Session["Phone"] = SAMR1.Phone;
+                Session["Gender"] = SAMR1.Gender;
+                Session["NID"] = SAMR1.NID;
+                Session["Address"] = SAMR1.Address;
+                
+              
+                return RedirectToAction("Regstration_S2");
 
+            }
+            else
+            {
+               // ViewBag.Message = "Not Valid!!";
+
+            }
+            return View();
+        }
+
+        [HttpGet]
         public ActionResult Regstration_S2()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Regstration_S2(S_Admin_Model_Reg02 SAMR2)
+        {
+            if (ModelState.IsValid)
+            {
+
+                Session["DOB"] = SAMR2.DOB.ToString();
+                Session["Security_key"] = SAMR2.Security_key;
+                Session["Password"] = SAMR2.Password;
+                Session["ConfirmPassword"] = SAMR2.ConfirmPassword;
+                if (SAMR2.IMG_file != null)
+                {
+                    string path = Server.MapPath("~/Img/ADMIN");
+                    string fileName = Path.GetFileName(SAMR2.IMG_file.FileName);
+                    string fullpath = Path.Combine(path, fileName);
+
+                    Session["IMG_file"] = fullpath;
+
+                }
+
+                
+
+
+                var SAMR1 = new S_Admin_Model_Reg01();
+                if (this.RegistrationCreate( SAMR1 , SAMR2))
+                {
+                    return RedirectToAction("login_S");
+
+
+                    // ViewBag.Message = "Data Inserted!!";
+                }
+                else
+                {
+                    // ViewBag.Message = "Something went wrong!!";
+                }
+            }
+            else
+            {
+                // ViewBag.Message = "not valid";
+            }
+            return View();
+        }
+
+
+
+        public bool RegistrationCreate(S_Admin_Model_Reg01 SAM1 , S_Admin_Model_Reg02 SAM2)
+        {
+
+            var admin = new Admin()
+            {
+                Ad_Email=Session["Email"].ToString(),
+                FirstName=Session["FirstName"].ToString(),
+                LastName=Session["LastName"].ToString(),
+                Address=Session["Address"].ToString(),
+                Phone=Session["Phone"].ToString(),
+                Security_key=Session["Security_key"].ToString(),
+                Gender=Session["Gender"].ToString(),
+                Nid=Session["NID"].ToString(),
+                Password=Session["Password"].ToString(),
+                Img=Session["IMG_file"].ToString()
+
+            };
+            var db = new bePartnerCentralDatabaseEntities2();
+            db.Admins.Add(admin);
+            try
+            {
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        [HttpGet]
         public ActionResult login_S()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult login_S(S_Admin_model_login SAML, string ReturnUrl)
+        {
+
+            var db = new bePartnerCentralDatabaseEntities2();
+            var admin = (from Id in db.Admins where Id.Ad_Email.Equals(SAML.Email) select Id).FirstOrDefault();
+            if (admin != null)
+            {
+                if (admin.Password.Equals(SAML.Password ))
+                {
+                    FormsAuthentication.SetAuthCookie(SAML.Email, true);
+                    Session.RemoveAll();
+                    Session["Email"] = SAML.Email;
+                    HttpCookie c1 = new HttpCookie("ADInfo");
+                    c1.Value = admin.Ad_Email.ToString();
+                    c1.Expires = DateTime.Now.AddDays(1);
+                    Response.Cookies.Add(c1);
+                    if (ReturnUrl != null)
+                    {
+                        string[] arr = ReturnUrl.Split('/');
+                        ViewBag.Message = arr[0].ToString();
+                        
+                        return RedirectToAction(arr[2].ToString(), arr[1].ToString());
+                    }
+                    return RedirectToAction("Index", "AD_Home");
+                }
+                else
+                {
+                    ViewBag.Message = "Incorrect Password.  ";
+                }
+                
+            }
+            else
+            {
+               ViewBag.Message = "Incorrect Email.";
+            }
+
+            return View(SAML);
+        }
+
     }
 }
