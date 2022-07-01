78b63bd9d829c6e3b969c1f0d7279ab1c4a5779c
sazzad ul alam Shawon <93247296+sazzadulalambd@users.noreply.github.com>
2022-07-02 04:26:46 +0600

    sazzad

---
+++ BePartner_App_Mid/Models/S_Admin_Model_Reg02.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BePartner_App_Mid.Models
+{
+    public class S_Admin_Model_Reg02
+    {
+
+        [Required(ErrorMessage = "*Date of birth")]
+        [CustomValidation.CustomValid]
+        public DateTime DOB { get; set; }
+
+        [Required(ErrorMessage = "*Security key")]
+        public string Security_key { get; set; }
+
+
+        [Required(ErrorMessage = "*Password required")]
+        [StringLength(50, MinimumLength = 7, ErrorMessage = "*Must be more than 8 characters.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "*Confirm Password required")]
+        [Compare("Password", ErrorMessage = "*Password not matching")]
+        public string ConfirmPassword { get; set; }
+
+
+        public HttpPostedFileBase IMG_file { get; set; }
+
+    }
+}
---
+++ BePartner_App_Mid/Models/S_Admin_model_login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BePartner_App_Mid.Models
+{
+    public class S_Admin_model_login
+    {
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "*Invalid Email")]
+        [Required(ErrorMessage = "*Enter Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "*Password ")]
+        [StringLength(50, MinimumLength = 7, ErrorMessage = "*Must be more than 8 characters.")]
+        public string Password { get; set; }
+    }
+}
